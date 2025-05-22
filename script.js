259e8d48cc8507cf65928e0021e159d0d207e57b
Noirnoir24 <184695851+Noirnoir24@users.noreply.github.com>
2025-05-22 09:33:30 -0600

    Create operate function,fix typos,add variables
    Fix a typo "substract" to "subtract",add operator,
    firstOperand and secondOperand variables,add an if
    statement to divide() to prevent the division of 0

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function add(firstDigit, secondDigit) {
     return firstDigit+secondDigit
 }
 
-function substract(firstDigit, secondDigit) {
+function subtract(firstDigit, secondDigit) {
     return firstDigit-secondDigit
 }
 
@@ -11,5 +11,27 @@ function multiply(firstDigit, secondDigit) {
 }
 
 function divide(firstDigit, secondDigit) {
+    if (secondDigit === 0) {
+        return "Cannot divide by zero dummy";
+    }
     return firstDigit/secondDigit
+}
+
+let operator
+let firstOperand
+let secondOperand
+
+function operate(operatorSign,operandOne,operandTwo) {
+switch(operatorSign){
+    case "+":
+       return add(operandOne,operandTwo);
+    case "-": 
+        return subtract(operandOne, operandTwo);
+    case "*":
+        return multiply(operandOne, operandTwo);
+    case "/": 
+    return divide(operandOne,operandTwo);
+    default: 
+    return "Invalid Operator";
+}
 }
