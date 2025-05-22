6d0b3e80eae8230601f69da25e6d8039c8675ac4
Noirnoir24 <184695851+Noirnoir24@users.noreply.github.com>
2025-05-22 09:03:43 -0600

    set html structure,link css&js, create functions
    Link external css and js files to html
    Add 4 functions each corresponding to an operation

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(firstDigit, secondDigit) {
+    return firstDigit+secondDigit
+}
+
+function substract(firstDigit, secondDigit) {
+    return firstDigit-secondDigit
+}
+
+function multiply(firstDigit, secondDigit) {
+    return firstDigit*secondDigit
+}
+
+function divide(firstDigit, secondDigit) {
+    return firstDigit/secondDigit
+}
